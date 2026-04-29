8d8571d2fa01c6ae736a30bf9c42b3f63707bab2
moaz123456 <mm6007@fayoum.edu.eg>
2026-04-29 17:20:43 +0300

    Creating the AppDbCotext and added the DbSets overridng the OnModelCreating

--- ِAppDbContextcs.cs
+++ ِAppDbContextcs.cs
@@ -1,6 +1,32 @@
-﻿namespace Smart_Medical_Appointment_System
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Smart_Medical_Appointment_System.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Smart_Medical_Appointment_System
 {
-    public class _ِAppDbContextcs
+    public class _ِAppDbContextcs : IdentityDbContext<IdentityUser>
     {
+       public DbSet<Doctor> Doctors { get; set; }
+       public DbSet<Patient> Patients { get; set; }
+       public DbSet<Appointment> Appointments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Appointment>()
+               .HasOne(a => a.Doctor)
+               .WithMany(d => d.Appointments)
+               .HasForeignKey(a => a.DoctorId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Appointment>()
+                .HasOne(a => a.Patient)
+                .WithMany(p => p.Appointments)
+                .HasForeignKey(a => a.PatientId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
+
     }
 }
